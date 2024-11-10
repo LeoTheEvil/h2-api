08d29fb66045ccbcdd7a70d379ee0a2c55831ea4
LeoTheEvil <iceninjashidomaru@gmail.com>
2024-11-10 15:59:34 -0300

    Imports corregidos.

--- src/main/java/com/h2/h2_api/OpenAPIConfiguration.java
+++ src/main/java/com/h2/h2_api/OpenAPIConfiguration.java
@@ -1,10 +1,13 @@
 package com.h2.h2_api;
 
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfiguration {
